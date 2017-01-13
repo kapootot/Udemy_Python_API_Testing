158ce5ed90f0995812719bc6be0a9d872c84fcb4
Amir Waisman <amir.waisman@onefootball.com>
2017-01-13 12:02:18 +0100

    Ongoing

--- tools/req.py
+++ tools/req.py
@@ -1,15 +1,15 @@
 from woocommerce import API
 
-class REQ():
 
+class REQ():
     def __init__(self):
         admin_consumer_key = "ck_0e16ac3057774f660f5b42f5263160033c800d0f"
         admin_consumer_secret = "cs_66019dffbf45add25636913ab54541aaba5100b4"
 
         self.wcapi = API(
             url="http://127.0.0.1/awstore/",
-            consumer_key = admin_consumer_key,
-            consumer_secret = admin_consumer_secret
+            consumer_key=admin_consumer_key,
+            consumer_secret=admin_consumer_secret
         )
 
     def test_api(self):
@@ -19,7 +19,6 @@ class REQ():
         """
 
         print(self.wcapi.get("").json())
-
 
     def post(self, endpoint, data):
         """
